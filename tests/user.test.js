4189529d5c87f1a5818cb624fadf05b1518ca085
Eder Ramirez <eder.ramirez87@gmail.com>
2019-07-16 16:08:42 -0500

    Testing and assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,7 +1,26 @@
-test('This is my first test case', () => {
-  1 + 2
+import { getFirstName, isValidPassword } from '../src/utils/user';
+
+test('Should return first name when given full name', () => {
+  const firstName = getFirstName('Eder Ramírez');
+  expect(firstName).toBe("Eder");
 });
 
-test('second test', () => {
+test('Should return first name when given first name', () => {
+  const firstName = getFirstName('Jen');
+  expect(firstName).toBe('Jen');
+});
 
+test('Shoul reject password shorter than 8 characters', () => {
+  const password = isValidPassword('pr123');
+  expect(password).toBeFalsy();
+});
+
+test('Shoul reject that password contains the word password', () => {
+  const password = isValidPassword('123password1..');
+  expect(password).toBeFalsy();
+});
+
+test('Shoul be a valid password', () => {
+  const password = isValidPassword('123456789');
+  expect(password).toBeTruthy();
 });
