1e52e7b04adf4878eac876bbbe949f5b7d907fa4
Eder Ramirez <eder.ramirez87@gmail.com>
2019-07-17 12:39:24 -0500

    Seeding the database

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,49 @@
 import 'cross-fetch/polyfill';
 import ApolloBoost, { gql } from 'apollo-boost';
+import bcrypt from 'bcryptjs';
 import prisma from '../src/prisma';
 
 const client = new ApolloBoost({
   uri: 'http://localhost:4000'
+});
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts();
+  await prisma.mutation.deleteManyUsers();
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: 'Jen',
+      email: 'jen@example.com',
+      password: bcrypt.hashSync('Red123456')
+    }
+  });
+
+  await prisma.mutation.createPost({
+    data: {
+      title: "Post 1",
+      body: "...",
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
+
+  await prisma.mutation.createPost({
+    data: {
+      title: "Post 2",
+      body: "...",
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
+
 });
 
 test("Should create a new user", async () => {
